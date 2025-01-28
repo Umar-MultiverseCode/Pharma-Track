da59fcab70bfa4e8bb0a23c857a7cde7407f089a
Umar Mulla <139649458+Umar-MultiverseCode@users.noreply.github.com>
2025-01-28 22:17:30 +0530

    Added urls.py, and login html

--- Pharma_app/views.py
+++ Pharma_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.http import JsonResponse
+ 
 
 # Create your views here.
+def home(request):
+    return render(request,'landing.html')
+
